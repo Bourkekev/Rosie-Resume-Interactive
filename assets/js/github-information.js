cac5552f878befa849bd8439fb0f297aa0c1c0d6
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-04-17 10:22:40 +0100

    remove the CORS anywhere url from getJSON as we have an error message now to display when api restricted

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -56,8 +56,8 @@ function fetchGitHubInformation(event) {
         </div>`);
 
     $.when(
-        $.getJSON(`https://cors-anywhere.herokuapp.com/https://api.github.com/users/${username}`),
-        $.getJSON(`https://cors-anywhere.herokuapp.com/https://api.github.com/users/${username}/repos`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         function(firstResponse, secondResponse){
             let userData = firstResponse[0];
@@ -73,6 +73,9 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`
                 );
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
