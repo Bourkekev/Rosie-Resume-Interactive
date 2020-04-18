649d5f9f28ba2aef0f4a86c1fb89369499df7083
Kevin <bourkekev@gmail.com>
2020-04-18 15:06:24 +0100

    Completion of email.js

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm){
+    let templateParams = {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "project_request": contactForm.projectsummary.value
+    };
+     
+    emailjs.send('gmail', 'rosie', templateParams)
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+        return false;
+}
